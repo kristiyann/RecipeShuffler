4bd3a6e3485693a6042f6844bc7a8322117854a4
kristiyan <kristiyan0312@abv.bg>
2021-12-27 22:54:12 +0200

    Fixed the output variable echoing outdated html

--- php/getRandomRecipe.php
+++ php/getRandomRecipe.php
@@ -9,19 +9,9 @@ if (isset($_POST['action'])) {
     $result = mysqli_query($conn, $sql);
 
     while($row=mysqli_fetch_assoc($result)) {
-        $output .= '<div class="col-sm-4 mb-3">
-        <div class="card border-secondary bg-light h-100" style="width: 18rem;">
-        <img src="'.$row['recipeThumbnail'].'" class="card-img-top" alt="The recipe image">
-        <div class="card-body d-flex flex-column">
-        <h5 class="card-title">'.$row['recipeName'].'</h5>
-        <div class="scrollable overflow-scroll">
-        <p class="card-text"><a>'.$row['recipeIngredients'].'</a> <br> <br> <a>'.$row['recipeSteps'].'</a></p> </div>
-        <a href="editRecipe.php?updateid='.$row['id'].'" class="btn btn-primary mt-auto">Edit</a> <a href="php/deleteRecipe.php?deleteid='.$row['id'].'" class="btn btn-danger">Delete</a>
-        </div>
-        </div>
-        </div>';
+        $output .= '<div class="col-sm-4 mb-3"> <div class="card border-secondary bg-light h-100" style="width: 18rem;"> <img src="'.$row['recipeThumbnail'].'" class="card-img-top" alt="The recipe image"> <div class="card-body d-flex flex-column"> <h5 class="card-title">'.$row['recipeName'].'</h5> <div class="box"> <p class="card-text mb-1">'.$row['recipeIngredients'].' <br> <br>'.$row['recipeSteps'].'</p> </div> <a href="editRecipe.php?updateid='.$row['id'].'" class="btn btn-primary mt-auto">Edit</a> <a href="php/deleteRecipe.php?deleteid='.$row['id'].'" class="btn btn-danger mt-1">Delete</a> </div> </div> </div>';
     }
-    echo $output;
+    echo nl2br($output);
 
     mysqli_close($conn);
 }
